06a8ae9931952fdc1aca57a44dcc3ec5ac457ec8
John Napiorkowski <jjn1056@yahoo.com>
2020-04-18 12:56:42 -0500

    let you declare validations inside the 'has'

--- example/login-profile/lib/Example/Model/Register.pm
+++ example/login-profile/lib/Example/Model/Register.pm
@@ -3,8 +3,16 @@ package Example::Model::Register;
 use Moo;
 use Valiant::Validations;
 
-has 'username' => (is => 'ro');
-has ['password', 'password_confirmation'] => (is => 'ro');
+has 'username' => (
+  is => 'ro',
+  validates => [presence=>1, length=>[3,24], format=>'alpha'],
+);
+
+has 'password' => (
+  is => 'ro',
+  validates => [presence=>1, length=>[6,24], confirmation=>1],
+);
+
 has 'first_name' => (is => 'ro');
 has 'last_name' => (is => 'ro');
 has 'address' => (is => 'ro');
@@ -12,8 +20,6 @@ has 'city' => (is => 'ro');
 has 'state' => (is => 'ro');
 has 'zip' => (is => 'ro');
 
-validates username => (presence=>1, length=>[3,24], format=>'alpha');
-validates password => (presence=>1, length=>[6,24], confirmation=>1);
 validates first_name => (presence=>1, length=>[2,24]);
 validates last_name => (presence=>1, length=>[2,48]);
 validates address => (presence=>1, length=>[2,48]);
@@ -25,13 +31,22 @@ sub ACCEPT_CONTEXT {
   my ($class, $c) = @_;
   my $model = $class->new($c->req->body_parameters);
   if($c->req->method eq 'POST') {
-    $model->validate;
-
     if($model->invalid) {
       use Devel::Dwarn;
       Dwarn +{ $model->errors->to_hash };
+    } else {
+      $c->model('Schema::Person')
+        ->create({
+          username => $model->username,
+          password => $model->password,
+          first_name => $model->first_name,
+          last_name => $model->last_name,
+          address => $model->address,
+          city => $model->city,
+          state => { name => $model->state },
+          zip => $model->zip,
+        });
     }
-    
   }
   return $model;
 }
