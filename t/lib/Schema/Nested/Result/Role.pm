2a97b728584c9f3ed73c7f76434f259422f809a6
John Napiorkowski <jjn1056@yahoo.com>
2020-12-28 15:10:47 -0600

    more testing for belongs to

---
+++ t/lib/Schema/Nested/Result/Role.pm
@@ -0,0 +1,25 @@
+package Schema::Nested::Result::Role;
+
+use strict;
+use warnings;
+
+use base 'Schema::Result';
+
+__PACKAGE__->table("role");
+
+__PACKAGE__->add_columns(
+  id => { data_type => 'integer', is_nullable => 0, is_auto_increment => 1 },
+  label => { data_type => 'varchar', is_nullable => 0, size => '24' },
+);
+
+__PACKAGE__->set_primary_key("id");
+__PACKAGE__->add_unique_constraint(['label']);
+
+__PACKAGE__->has_many(
+  person_roles =>
+  'Schema::Nested::Result::PersonRole',
+  { 'foreign.role_id' => 'self.id' }
+);
+
+
+1;
