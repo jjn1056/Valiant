ba7d0ed7b36cb8f20d2ce3339eaff9935d871059
John Napiorkowski <jjn1056@yahoo.com>
2021-01-11 17:09:53 -0600

    codecov badge

--- lib/Valiant.pm
+++ lib/Valiant.pm
@@ -13,6 +13,7 @@ Valiant - Ruby on Rails-like validation framework.
 
 <a href="https://github.com/jjn1056/valiant/actions"><img src="https://github.com/jjn1056/valiant/workflows/linux/badge.svg"></a>
 <a href="https://metacpan.org/pod/Valiant"><img src="https://badge.fury.io/pl/Valiant.svg"></a>
+<a href="https://codecov.io/github/jjn1056/Valiant/?branch=master"><img alt="Coverage" src="https://codecov.io/github/jjn1056/Valiant/coverage.svg?branch=master"></a>
 
 =end html
 
