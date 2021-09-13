56a282d25ac0bd797593f88e495547cd6546a23c
John Napiorkowski <jjn1056@yahoo.com>
2021-09-13 12:38:13 -0500

    proposed fix

--- t/lib/OP/Issue/Importer.pm
+++ t/lib/OP/Issue/Importer.pm
@@ -10,10 +10,10 @@ sub import {
   my $class = shift;
   my $target = caller;
 
-  Moo::Role->apply_roles_to_package($target, 'MooRole')
-    unless Role::Tiny::does_role($target, 'MooRole');
-
-  my $test = $target->can('test');
+  Moo::Role->apply_roles_to_package($target, 'MooRole');
+  #   unless Role::Tiny::does_role($target, 'MooRole');
+  #  my $test = $target->can('test');
+  my $test = defined &{"${target}::test"} ? \&{"${target}::test"} : undef;
   Moo::_Utils::_install_tracked($target, 'test', sub { $test->($target,@_) })
     if $test;
 } 
