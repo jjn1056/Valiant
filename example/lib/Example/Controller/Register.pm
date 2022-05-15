bb281d0ed898dbfe8295c00ba266f4a4a87e46c6
John Napiorkowski <jjn1056@yahoo.com>
2022-05-15 12:31:55 -0500

    tweak example

--- example/lib/Example/Controller/Register.pm
+++ example/lib/Example/Controller/Register.pm
@@ -7,28 +7,28 @@ use Example::Syntax;
 extends 'Catalyst::Controller';
 
 sub root :Chained(/root) PathPart(register) Args(0) Does(Verbs)  ($self, $c) {
-  $c->redirect_to_action('#home') if $c->user;
+  return $c->redirect_to_action('#home') if $c->user;
+  my $person =  $c->model('Schema::Person')->new_result(+{});
+  my $view = $c->view('Components::Register', person=>$person);
+  return $person, $view;
 }
 
-  sub GET :Action ($self, $c) {
-    my $person = $c->model('Schema::Person')
-      ->new_result(+{});
-    return $c->view('Components::Register', person=>$person)->http_ok;
+  sub GET :Action ($self, $c, $person, $view) {
+    return $view->http_ok;
   }
 
-  sub POST :Action ($self, $c) {
+  sub POST :Action ($self, $c, $person, $view) {
     my %params = $c->structured_body(
       ['person'], 
       'username', 'first_name', 'last_name', 
       'password', 'password_confirmation'
     )->to_hash;
 
-    my $person = $c->model('Schema::Person')
-      ->create(\%params);
+    $person->set_columns_recursively(\%params)->insert;
 
-    return $c->redirect_to_action('#login') if $person->valid;
-    return $c->view('Components::Register', person=>$person)->http_bad_request;
+    return $person->valid ?
+      $c->redirect_to_action('#login') :
+        $view->http_bad_request;
   }
 
 __PACKAGE__->meta->make_immutable;
-
