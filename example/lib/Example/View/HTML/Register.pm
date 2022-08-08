a7fded5b4f6254dd29a42f384d6a97c70f3fa4b1
John Napiorkowski <jjn1056@yahoo.com>
2022-08-08 18:11:46 -0500

    typo

--- example/lib/Example/View/HTML/Register.pm
+++ example/lib/Example/View/HTML/Register.pm
@@ -6,11 +6,11 @@ use Valiant::HTML::TagBuilder 'div', 'a', 'fieldset';
 
 extends 'Example::View::HTML';
 
-has 'registration' => (is=>'ro', required=>1);
+has 'registration' => (is=>'ro', required=>1, handles=>[qw/form/]);
 
 sub render($self, $c) {
   $c->view('HTML::Layout', page_title=>'Homepage', sub($layout) {
-    $self->registration->form( +{style=>'width:35em; margin:auto', theme=>$self->theme}, sub ($reg, $fb) {
+    $self->form( +{style=>'width:35em; margin:auto', theme=>$self->theme}, sub ($reg, $fb) {
       fieldset [
         $fb->legend,
         div +{ class=>'form-group' }, $fb->model_errors,
@@ -18,11 +18,11 @@ sub render($self, $c) {
         div +{ class=>'form-group' }, $reg->last_name,
         div +{ class=>'form-group' }, $reg->username,
         div +{ class=>'form-group' }, $reg->password,
-        div +{ class=>'form-group' }, $reg->password_confirmation
+        div +{ class=>'form-group' }, $reg->password_confirmation,
 
         $fb->submit('Register for Account', +{class=>'btn btn-lg btn-primary btn-block'}),
       ],
-      div { style=>'text-align:center' }, a { href=>'/login' }, "Login to existing account."
+      div { class=>'text-center ' }, a { href=>'/login' }, "Login to existing account."
     }),
   });
 }
