521159d046a2d4695689f76fab3b5dc4c944929a
John Napiorkowski <jjn1056@yahoo.com>
2022-12-09 09:52:08 -0600

    fix contacts query session info

--- example/lib/Example/Model/ContactsQuery/Session.pm
+++ example/lib/Example/Model/ContactsQuery/Session.pm
@@ -10,8 +10,8 @@ with 'Catalyst::Component::InstancePerContext';
 has page => (is=>'ro', required=>1, default=>1); 
 
 sub build_per_context_instance($self, $c, $q) {
-  my $args = merge( $q->nested_params, ($c->model('Session')->todo_query //+{}) );
-  $c->model('Session')->todo_query($args);
+  my $args = merge( $q->nested_params, ($c->model('Session')->contacts_query //+{}) );
+  $c->model('Session')->contacts_query($args);
   return ref($self)->new(%$args);
 }
 
