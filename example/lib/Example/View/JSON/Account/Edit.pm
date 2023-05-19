cc04d670568573db742d35c8d255753c03a0c0b4
John Napiorkowski <jjn1056@yahoo.com>
2023-05-19 18:18:20 -0500

    first pass JSON model

--- example/lib/Example/View/JSON/Account/Edit.pm
+++ example/lib/Example/View/JSON/Account/Edit.pm
@@ -1,30 +1,95 @@
 package Example::View::JSON::Account::Edit;
  
 use Moose;
- 
-extends 'Catalyst::View::BasePerRequest';
-use JSON::MaybeXS;
+use Example::Syntax;
+
+extends 'Catalyst::View::JSONBuilder';
 
 has account => (is=>'ro', required=>1);
 
- 
-sub render {
+sub has_attribute_for_json {
+  my ($self, $model, $name) = @_;
+  return 0;
+}
+
+sub build_json {
   my ($self, $c) = @_;
-  my $errors = $self->account->errors->as_json;
-  my $response =  +{
-    $self->account->get_columns,
-    errors => $errors,
-  };
+  my $jb = $self->json_builder('account'); # if value doesn't match a model or is empty , create empty model at this namespace
 
-  use Devel::Dwarn;
-  Dwarn $response;
+  return $jb->string('username', {value=>undef, omit_undef=>1})
+    ->string('first_name', {name=>'first-name', value=>'1111'})
+    ->string('empty', {value=>'abc', omit_empty=>1})
+    ->string('tokena', {value=>"ssss", name=>'token-a'})
+    ->string('token', "sdfsdfsdfsdfsdf")
+    ->object('profile', +{namespace=>'me'}, sub($v, $jb, $profile) {
+      $jb->string('address')
+        ->if(1, sub($v, $jb) { $jb->string('city', $jb->current_model->address) })
+        ->if(sub {0}, sub($v, $jb) { $jb->string('city', $jb->current_model->address) })  
+        ->number('state_id') 
+    })
+    ->object('profile', +{namespace=>'empty-profile', omit_empty=>1}, sub($v, $jb, $profile) {})
+    ->object('profile', +{namespace=>'empty-profile-ok'}, sub($v, $jb, $profile) {}) 
+    ->object('profile', sub($v, $jb, $profile) { 
+      $jb->string('address')
+        ->string('city')
+        ->number('state_id', {name=>'state-id'})
+        ->string('zip', {value=>undef})
+        ->string('phone_number')
+        ->string('birthday', sub ($v, $bd) { $bd->ymd })
+        ->string('status')
+        ->boolean('registered')
+        ->number('employment_id')
+        ->object($self->account->profile, sub ($v, $jb, $p) {
+          $jb->string('address')
+            ->string('city')
+        })
+        ->object($self->account->profile, {namespace=>'pro1'}, sub ($v, $jb, $p) {
+          $jb->string('address')
+            ->string('city')
+        })        
+    })
+    ->string('last_name')
+    ->array('person_roles', +{namespace=>'empty-pr', omit_empty=>1}, sub($v, $jb, $person_role) {})
+    ->array('person_roles', sub($v, $jb, $person_role) {
+      $jb->number('role_id');
+    })
+    ->array('credit_cards', +{namespace=>'credit-cards'}, sub($v, $jb, $credit_card) {
+      $jb->number('id')
+        ->string('card_number');
+        #->date('expiration')
+    });
 
-  return encode_json($response);
+    # ->model() set current model until end, if hashref make that into a model
 }
  
 __PACKAGE__->config(
-  content_type=>'application/json',
   status_codes=>[200,400]
 );
  
 __PACKAGE__->meta->make_immutable();
+
+__END__
+
+  $jb->username_str
+    ->first_name_str
+    ->str::last_name
+    ->profile(sub($jb, $profile) {
+      $jb->address
+        ->city
+        ->state_id
+        ->zip
+        ->phone_number
+        ->birthday
+        ->status
+        ->registered
+        ->employment_id;
+    })
+    ->person_roles(sub($jb, $person_role) {
+      $jb->role_id;
+    })
+    ->credit_cards(sub($jb, $credit_card) {
+      $jb->id
+        ->card_number
+        ->expiration
+        ->_delete;
+    });
