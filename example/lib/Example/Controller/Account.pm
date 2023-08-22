ff19aaae712757222fd739daa59d8c04b7f3c566
John Napiorkowski <jjn1056@yahoo.com>
2023-08-22 18:19:54 -0500

    docs

--- example/lib/Example/Controller/Account.pm
+++ example/lib/Example/Controller/Account.pm
@@ -6,10 +6,6 @@ use Example::Syntax;
 
 extends 'Example::Controller';
 
-sub update_account_uri($self, $model_path, @args) {
-  return $self->update_uri(@args);
-}
-
 sub root :At('$path_end/...') Via('../protected')  ($self, $c, $user) {
   $c->action->next(my $account = $user->account);
 }
@@ -25,6 +21,12 @@ sub root :At('$path_end/...') Via('../protected')  ($self, $c, $user) {
     # PATCH /account
     sub update :Patch('') Via('prepare_edit') BodyModel ($self, $c, $account, $bm) {
       return $account->set_from_request($bm);
+    }
+
+    # Used by the form_for helper in the view. Since this controller doesn't 
+    # define a 'find' action we need to discard the $model_path argument.
+    sub update_account_uri($self, $model_path, @args) {
+      return $self->update_uri(@args);
     }
 
 __PACKAGE__->meta->make_immutable;
